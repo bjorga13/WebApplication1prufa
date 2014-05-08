6910f16dcdc64fdf4bd74b8c827c9fc06c88f56a
bjorga13 <bjorga13@ru.is>
2014-05-08 12:37:03 +0000

    Added first version of project

---
+++ WebApplication1prufa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication1prufa.Startup))]
+namespace WebApplication1prufa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
